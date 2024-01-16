49440466a4bf5e589bc96c6bf0b5141a486853cc
thomasasia <thomas.harriett@gmail.com>
2024-01-15 23:56:11 -0500

    added plutonium

--- item_fluid_defs.lua
+++ item_fluid_defs.lua
@@ -140,3 +140,37 @@ local ash_sludge = {{
 }}
 data:extend(ash_sludge)
 
+local plutonium = {
+    {
+    type = "item",
+    name = "tbp-plutonium",
+    icon = "__toms-byproducts__/graphics/icons/plutonium.png",
+    pictures =
+    {
+      layers =
+      {
+        {
+          size = 64,
+          filename = "__toms-byproducts__/graphics/icons/plutonium.png",
+          scale = 0.25,
+          mipmap_count = 4
+        },
+        {
+          draw_as_light = true,
+          blend_mode = "additive",
+          size = 64,
+          filename = "__toms-byproducts__/graphics/icons/plutonium.png",
+          scale = 0.25,
+          tint = {r = 0.3, g = 0.3, b = 0.3, a = 0.3},
+          mipmap_count = 4
+        }
+      }
+    },
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "intermediate-product",
+    order = "r[plutonium]",
+    stack_size = 100
+  }
+}
+data:extend(plutonium)
+
