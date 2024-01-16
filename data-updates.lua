7dd56cb67713406e77cede07bde38907a1fbe51c
thomasasia <thomas.harriett@gmail.com>
2024-01-15 20:43:21 -0500

    explanatory comments

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,5 @@
 --data-updates.lua
 
+-- the recipe changes go in data-updates.lua because most other mods will modify recipes in data.lua.
+-- this way regardless if the mod is loaded before or after other mods, it should add the byproducts correctly, and without harming other mods' functionality
 require("recipe_overrides")
