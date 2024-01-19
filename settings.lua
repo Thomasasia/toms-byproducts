373a9946832c001b0a789b7ba1a9256b89557557
thomasasia <thomas.harriett@gmail.com>
2024-01-18 19:04:56 -0500

    settings

--- settings.lua
+++ settings.lua
@@ -1,1 +1,46 @@
 -- settings.lua
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "tbp-enable-slag",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-spent-etchant",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-plastic-waste",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-copper-sulfate",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-petrolium-sludge",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-ash-sludge",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tbp-enable-plutonium",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+})
